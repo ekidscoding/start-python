de41f28855fe6e33cf8332a8f1f43b5b1f895c65
Kiril Chervoniak <42374203+kirilche@users.noreply.github.com>
2025-07-23 11:13:49 +0300

    feat: add new section

---
+++ examples/good.py
@@ -0,0 +1,10 @@
+print('Вам пропонується ввести 2 цілих числа,')
+print('після вводу числа натисніть "Enter"')
+number1 = int(input("Введіть перше ціле число: "))
+number2 = int(input("Введіть друге ціле число: "))
+
+summa = number1 + number2
+riznytsia = number1 - number2
+print()
+print(f"Сума чисел {number1} та {number2} становить {summa}")
+print(f"Різниця чисел {number1} та {number2} становить {riznytsia}")
